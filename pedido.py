d830f5cdf7dc85fbb80bb440693e5c5234f11a85
Martin Sonda <martindanielsonda@gmail.com>
2026-06-22 01:13:48 -0300

    fix: agregar atributo repartidor en Pedido

--- pedido.py
+++ pedido.py
@@ -1,18 +1,20 @@
 from datetime import date
 from detalle_pedido import DetallePedido
+from empleado import Empleado
 from enums import EstadoPedido
 from participante import Participante
 from producto import Producto
 from usuario import Usuario
 
 class Pedido:
-    def __init__(self, id: int, fecha: str, estado: EstadoPedido, total: float, cliente: Participante, usuario: Usuario):
+    def __init__(self, id: int, fecha: str, estado: EstadoPedido, total: float, cliente: Participante, usuario: Usuario, repartidor: Empleado = None):
         self.id = id
         self.fecha = fecha
         self.estado = estado
         self.total = total
         self.cliente = cliente # Puede ser PersonaFisica o Empresa
         self.usuario = usuario
+        self.repartidor = repartidor # Opcional: se asigna al organizar la entrega.
         self.detalles: list[DetallePedido] = [] 
 
     def agregarProducto(self, producto: Producto, cantidad: int):
